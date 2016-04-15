1b98f1e79ad4c91cdd405a48ed9aecdaf9d92b90
Jr Reyes <jr.evilboss@gmail.com>
2016-04-15 17:15:21 +0800

    out to cs

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -20,9 +20,12 @@ let getTagId = function (result) {
     return tagId;
   }
 }
-let callActionTag = function (tagId) {
+let callAction = function (url) {
+  console.log('Reading posts');
+
 
 }
+
 Template.home.helpers({
   message: function () {
     return state.get('message');
@@ -39,12 +42,25 @@ Template.home.created = function () {
 };
 
 Template.home.onRendered(function () {
+
+  let url = 'http://www.actiontag.io/api/82d59db7-61b1-4d4d-88fe-05f8a832bd3a.json';
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      console.log(xhr.responseText);
+    }
+  }
+  xhr.send();
   if (Meteor.isCordova) {
+
+
+
     Meteor.startup(function () {
+      console.log('cordova started!');
       cordova.plugins.barcodeScanner.scan(
         function (result) {
           let tagId = getTagId(result);
-
         },
         function (error) {
           console.log("Scanning failed: " + error);
--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd build/production/android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 release-unsigned.apk action-scan
-$ANDROID_HOME/build-tools/23.0.1/zipalign 4 \release-unsigned.apk action-scan.apk
+ Scan
